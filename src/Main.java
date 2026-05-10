43fef5b349bbbf27433c97478030a9460faeb3f1
1nsp1re133 <eherna229@gmail.com>
2026-05-09 21:18:04 -0700

    revise name input popup to include hero type selection

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import controller.DungeonAdventure;
 import model.Room;
 import view.GameView;
 
 public class Main {
     public static void main(String[] args) {
         new GameView();
+        //DungeonAdventure myController = new DungeonAdventure(new GameView());
     }
 }
